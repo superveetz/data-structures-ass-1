a1ec0a07441ea3627b26c85cbef2777bacc8e0de
Alex Di Vito <Alex Di Vito>
2017-05-09 20:28:50 -0700

    first commit

---
+++ src/Node.cpp
@@ -0,0 +1,19 @@
+#ifndef NODE_H
+#define NODE_H
+
+using namespace std;
+
+class Node
+{
+    private:
+
+    public:
+        int x;
+        Node* next;
+
+        Node() {
+            
+        }
+};
+
+#endif  // NODE_H
